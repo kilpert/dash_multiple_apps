a88ea6be376249e4f6747622398a6ad21154c4be
kilpert <fkilpert@gmail.com>
2022-09-05 09:34:56 +0200

    use import_module for app imports

--- index.py
+++ index.py
@@ -1,12 +1,23 @@
+import os
+import glob
+from importlib import import_module
+import json
+
 import dash
 from dash import Dash, html, dcc
 from dash.dependencies import Input, Output
 
 from app import app
-from apps import app1
-from apps import app2
-from apps import app3
-from apps import app4
+
+
+app_layouts = {}
+
+apps = [ os.path.basename(os.path.dirname(x)) for x in glob.glob("apps/**/app.py")]
+print("Apps:", apps )
+for a in apps:
+    print("{:#^60}".format(f" Module: {a} "))
+    m = import_module(f"apps.{a}.app")
+    app_layouts[a] = m.app_layout()
 
 
 app.layout = html.Div([
@@ -18,18 +29,10 @@ app.layout = html.Div([
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
-    print(pathname)
-
-    if pathname == '/apps/app1':
-        return app1.app_layout()
-    elif pathname == '/apps/app2':
-        return app2.app_layout()
-    elif pathname == '/apps/app3':
-        return app3.app_layout()
-    elif pathname == '/apps/app4':
-        return app4.app_layout()
-    else:
-        return '404'
+    try:
+        return app_layouts[os.path.basename(pathname)]
+    except:
+        return "404"
 
 
 if __name__ == '__main__':
