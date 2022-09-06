d4b4c9c0e7e1dd94775f4802d4590aa926073c44
kilpert <fkilpert@gmail.com>
2022-09-06 10:47:31 +0200

    working apps in subfolders

--- index.py
+++ index.py
@@ -3,31 +3,45 @@ import glob
 from importlib import import_module
 import json
 import subprocess
+import datetime
+import re
 
 import dash
 from dash import Dash, html, dcc
 from dash.dependencies import Input, Output
 
-from application import app
+from application import app 
 
 import apps # modules
 
 
+def find_apps(path, name, maxdepth=1):
+    """Find filename (e.g. 'App.py') in folder using 'find'."""
+    paths = subprocess.check_output(f"find '{path}' -maxdepth {maxdepth} -name '{name}'", shell=True).decode().split()
+    apps_list = []
+    for p in paths:
+        p = re.sub(f"^{path}", "", p)
+        p = re.sub(f"/{name}$", "", p)
+        apps_list.append(p)
+    return apps_list
+
+
 ## Apps ##
-app_list = [ os.path.basename(os.path.dirname(x)) for x in glob.glob("apps/**/App.py")]
+##apps_list = [ os.path.basename(os.path.dirname(x)) for x in glob.glob("apps/**/App.py")]
+apps_list = [a.replace("/", ".") for a in find_apps("apps/", "App.py", 3)]
 print("{:#^60}".format(f" Apps "))
-print(app_list, len(app_list))
+print(apps_list, len(apps_list))
 
 
 print("{:#^60}".format(f" App Modules "))
-for a in app_list:
+for a in apps_list:
     m = f"apps.{a}.App"
     print(m)
     import_module(m)
 
 
-##print("{:#^60}".format(f" globals() "))
-##print(globals())
+print("{:#^60}".format(f" globals() "))
+print(globals())
 
 
 ## Layout ##
@@ -42,15 +56,22 @@ print("{:#^60}".format(f" Requests "))
               [Input('url', 'pathname')])
 def display_page(pathname):
     a = os.path.basename(pathname)
-    print("App:", f"'{a}'")
+    ##print("m:", m)
+
+    ## module "path"
+    m = pathname.replace("/", ".")
+    m = re.sub("^\.", "", m)
+    ##print("m:", m)
+
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"{now}\tApp: '{a}' ")
     try:
-        if a=="":
-            return eval(f"apps.landing_page.App.app_layout()")
-        else:
-            print(eval(f"apps.{a}.App.app_layout()"))
-            return eval(f"apps.{a}.App.app_layout()")
-            print(eval(f"apps.{a}.App.app.layout"))
-            return eval(f"apps.{a}.App.app.layout")
+        if m=="":
+            m = "landing_page"
+
+        layout = f"apps.{m}.App.app_layout()"
+        ##print("layout:", layout)
+        return eval(layout)
     except:
         return "404" 
 
